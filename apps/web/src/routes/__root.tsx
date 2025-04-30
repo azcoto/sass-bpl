a1d6b4bfb3a8ef3a773774326579ef700d4b7558
Arizal Coto <rizal.zc@gmail.com>
2025-05-01 00:36:49 +0700

    fix: lint issue

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -120,7 +120,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
             Deferred
           </Link>{' '}
           <Link
-            // @ts-expect-error
+            // @ts-expect-error check why
             to="/this-route-does-not-exist"
             activeProps={{
               className: 'font-bold'
