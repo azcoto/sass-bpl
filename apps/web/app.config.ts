01f128bd32c03b2925b56f0f74deca47c3a479c0
Arizal Coto <rizal.zc@gmail.com>
2025-05-01 01:52:23 +0700

    refactor: streamline vite plugin

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from '@tanstack/react-start/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   tsr: {
     appDirectory: 'src'
   },
   vite: {
-    plugins: [
-      tsConfigPaths({
-        projects: ['./tsconfig.json']
-      })
-    ]
+    plugins: [tsConfigPaths(), tailwindcss()]
   }
 });
